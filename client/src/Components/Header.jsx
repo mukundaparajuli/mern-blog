e4520482d9f7fd861abfc3ef4b85f4977e750353
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-05-07 00:01:00 +0545

    configured tailwind css

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div
+      className=""
+      style={{
+        padding: "20px",
+        height: "20px",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "start",
+      }}
+    >
+      <div>Logo</div>
+      <div>Profile</div>
+    </div>
+  );
+};
+
+export default Header;
