736130c8452da178a8a3325701cbcc4d810c9eec
Mukunda Parajuli <129422610+mukundaparajuli@users.noreply.github.com>
2024-10-21 14:40:42 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const bodyParser = require("body-parser")
 databaseConnection();
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://techtonic-frontend.onrender.com/',
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
     allowedHeaders: ['Content-Type']
